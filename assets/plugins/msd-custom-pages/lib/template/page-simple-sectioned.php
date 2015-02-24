6b316b97c19a099f6adc443006e547ef0de6c15b
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2015-02-24 13:43:27 -0500

    move output

--- assets/plugins/msd-custom-pages/lib/template/page-simple-sectioned.php
+++ assets/plugins/msd-custom-pages/lib/template/page-simple-sectioned.php
@@ -4,7 +4,7 @@ Template Name: Simple Sectioned Page
 */
 if(function_exists('genesis')){
     //this is a genesis themed site
-    add_action('genesis_loop',array('MSDSimpleSectionedPage','sectioned_page_output'),30);
+    add_action('genesis_before_footer',array('MSDSimpleSectionedPage','sectioned_page_output'),0);
     add_action('wp_print_footer_scripts',array('MSDSimpleSectionedPage','sectioned_page_floating_nav'));
     genesis();
 } else {
